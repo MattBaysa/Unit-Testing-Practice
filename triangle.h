d23a78380c0298caa3b5957f7bff85311f056b03
MattBaysa <mattbaysa50@gmail.com>
2022-03-08 10:33:38 -0800

    Create Unit Testing Test

---
+++ triangle.h
@@ -0,0 +1,12 @@
+//
+// Created by Matthew Thayer on 3/7/22.
+//
+
+// triangle.h - Determines the type of triangle based on the three sides.
+
+// Returns the triangle type based on the length of the three sides:
+// 'e' : Equilateral triangle
+// 'i' : Isosceles triangle
+// 's' : Scalene triangle
+// 'n' : Not a triangle
+char triangleType(int side1, int side2, int side3);
